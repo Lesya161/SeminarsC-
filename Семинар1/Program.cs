ae6c70f42f449313207f8e1eb822db233321608f
Lesya <neptun96@mail.ru>
2022-10-01 20:27:17 +0300

    4 приемр

--- Семинар1/Program.cs
+++ Семинар1/Program.cs
@@ -54,3 +54,19 @@ current++;
 3162 / 100 = 31
 3162 / 1000 = 3
 */
+
+// Задача 3. Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает последнюю цифру этого числа.
+
+Console.Write("Input a three-digit namber: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n >= 100 && n < 1000)
+{
+    int lastDigit = n % 10;
+    Console.WriteLine($"Last digit of {n} is {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Uncorrect input!");
+}
