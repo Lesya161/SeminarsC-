99b50205a9b69c2431c649d93b10af5bf40f0b59
Lesya <neptun96@mail.ru>
2022-10-01 20:09:06 +0300

    2 пример

--- Семинар1/Program.cs
+++ Семинар1/Program.cs
@@ -1,4 +1,25 @@
-﻿Console.Write("Input a number: ");
+﻿/*
+Console.Write("Input a number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Your number is " + num);
+*/
+
+// Напишите программу, которая на вход принимает два числа и проверяет, 
+//является ли первое число квадратом второго.
+
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int q2 = n2 * n2;
+
+if(n1 == q2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
