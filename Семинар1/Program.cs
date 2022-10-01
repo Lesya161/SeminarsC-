3611bbd0ede68e5ef0575f5177bf845a782cbc4f
Lesya <neptun96@mail.ru>
2022-10-01 20:58:10 +0300

    ДЗ 1 задача

--- Семинар1/Program.cs
+++ Семинар1/Program.cs
@@ -70,4 +70,35 @@ else
 {
     Console.WriteLine("Uncorrect input!");
 }
+*/
+
+// Домашка!!!
 /*
+
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+if(n1 > n2)
+{
+    max = n1;
+}
+else
+{
+    max = n2;
+}
+
+int min = n1;
+if(n1 > n2)
+{
+    min = n2;
+}
+else
+{
+    min = n1;
+}
+Console.WriteLine($"max = {max} and min = {min}");
+
+*/
