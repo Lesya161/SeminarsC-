0d94fca808b1183f6d66341494f197b0d5dd2f24
Lesya <neptun96@mail.ru>
2022-10-05 22:05:38 +0300

    вторая задача

--- Семинар2/Program.cs
+++ Семинар2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая принимает двузначное число и показывает наибольшую цифру числа.
-
+/*
 int FindBiggerDigit(int num)
 {
     int result;
@@ -24,3 +24,20 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int biggerDigit = FindBiggerDigit(number);
 Console.WriteLine($"Bigger digit of {number} is {biggerDigit}");
+*/
+
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int RemoveSecond(int num)
+{
+    int ed = num % 10;
+    int doc = num / 100;
+    int result = doc * 10 + ed;
+    return result;
+}
+
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+Console.WriteLine(RemoveSecond(number));
+
+// Напишите программу, которая принимает на вход число n и проверяет, кратно ли оно одновременно a и b(целочисленные делители, также задаются пользователем).
