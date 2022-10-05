f52fa4e51ab4799f9aaaee29e399ea0eebed8b41
Lesya <neptun96@mail.ru>
2022-10-05 22:32:56 +0300

    третья задача

--- Семинар2/Program.cs
+++ Семинар2/Program.cs
@@ -41,7 +41,7 @@ Console.WriteLine(number);
 Console.WriteLine(RemoveSecond(number));
 */
 // Напишите программу, которая принимает на вход число n и проверяет, кратно ли оно одновременно a и b(целочисленные делители, также задаются пользователем).
-
+/*
 bool Del(int num, int num1, int num2)
 {
     if(num % num1 == 0 && num % num2 == 0)
@@ -57,6 +57,7 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второй делитель: ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Ответ: {Del(number,a,b)}");
+*/
 // либо
 // bool Suc = Del(number,a,b);
 // Console.WriteLine($"Ответ: {Suc}");
