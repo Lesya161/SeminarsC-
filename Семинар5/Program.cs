9979bae8bef96ebe01abec8b42f012b055b34bf5
Lesya <neptun96@mail.ru>
2022-10-16 20:17:23 +0300

    5 дз

--- Семинар5/Program.cs
+++ Семинар5/Program.cs
@@ -14,7 +14,7 @@ int[] NewArray(int size)
 */
 
 //Универвальные методы для того чтобы задать массив и его показать
-
+/*
 int[] CreateArray(int size, int minValue, int maxValue)
 {
     int[] array = new int [size];
@@ -31,7 +31,7 @@ void ShowArray(int[] array)
     
     Console.WriteLine();
 }
-
+*/
 
 //Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных элементов массива.
 /*
@@ -109,7 +109,7 @@ Console.WriteLine("Число двузначных эдементов масси
 */
 
 //Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
-
+/*
 bool YourNumber(int[] array, int num)
 {
     for(int i = 0; i < array.Length; i++)
@@ -131,9 +131,122 @@ ShowArray(endArray);
 Console.Write("Введите ваше число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Ваше число есть в массиве? " + YourNumber(endArray, number));
-
+*/
 //после инт намбер
 //if(YourNumber(endArray, number))
 //    Console.WriteLine("Твое число есть в массиве");
 //else
 //    Console.WriteLine("Твоего числа нет в массиве");
+
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int [size];
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array [i] + " ");
+    
+    Console.WriteLine();
+}
+
+//1
+/*
+int ShowEvenElementsNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+Console.Write("Input a numbers of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Minimal number of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Maximal number of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] threeDigitArray = CreateRandomArray(size, min, max);
+ShowArray(threeDigitArray);
+int result = ShowEvenElementsNumbers(threeDigitArray);
+Console.WriteLine($"In your array {result} even elements.");
+*/
+
+
+//2
+/*
+int SumElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+        sum += array[i];
+    return sum;
+}
+
+Console.Write("Input a numbers of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Minimal number of elements: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Maximal number of elements: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = CreateRandomArray(size, min, max);
+ShowArray(randomArray);
+int result = SumElements(randomArray);
+Console.WriteLine($"The sum of your array in uneven elements is {result}.");
+*/
+
+//3
+/*
+double[] CreateDoubleArray(int size)
+{
+    double[] array = new double [size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Write element of array: ");
+        array[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowDoubleArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array [i] + " ");
+    
+    Console.WriteLine();
+}
+
+double FindMinMax(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    double result = 0;
+
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] > max) max = array[i];
+
+    for(int i = 0; i < array.Length; i++)
+        if(array[i] < min) min = array[i];
+
+    result = max - min;
+    result = Math.Round(result, 2);
+    return result;
+}
+
+Console.Write("Input a numbers of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] endArray = CreateDoubleArray(size);
+ShowDoubleArray(endArray);
+double differens = FindMinMax(endArray);
+
+Console.WriteLine($"In your array differens max and mun is {differens}.");
+*/
